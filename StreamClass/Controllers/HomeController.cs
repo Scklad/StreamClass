cc9a99b1e4695472c609f814c9cb3b70a351405f
Thomas Floret <tfloret@cciformation-seineestuaire.fr>
2020-03-26 23:23:04 +0100

    Accès au contenu streaming

--- StreamClass/Controllers/HomeController.cs
+++ StreamClass/Controllers/HomeController.cs
@@ -1,15 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using StreamClass.Models;
+using System.Diagnostics;
+using System.IO;
 
 namespace StreamClass.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IHostingEnvironment _variable_env;
+        private readonly string _cheminVideo = "\\Videos\\video.mp4";
+
+        public HomeController(IHostingEnvironment variableEnv)
+        {
+            _variable_env = variableEnv;
+        }
+
         public IActionResult Index()//test
         {
             return View();
@@ -20,10 +26,23 @@ namespace StreamClass.Controllers
             return View();
         }
 
+        public IActionResult Video()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult GetVideo()
+        {
+            string cheminVideoComplet = _variable_env.ContentRootPath + _cheminVideo;
+
+            FileStream stream = new FileStream(cheminVideoComplet, FileMode.Open);
+            return new FileStreamResult(stream, "video/mp4");
         }
     }
 }
