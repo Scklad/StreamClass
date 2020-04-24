61f240ee0a8a20a93fb5b2b06e54e84879952951
Sckald <damienchereault@gmail.com>
2020-04-24 17:02:40 +0200

    integration streaming master

--- StreamClass/Controllers/HomeController.cs
+++ StreamClass/Controllers/HomeController.cs
@@ -8,12 +8,17 @@ using Microsoft.AspNetCore.Mvc;
 using StreamClass.Models;
 using System.Diagnostics;
 using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
 namespace StreamClass.Controllers
 {
     [Authorize]
     public class HomeController : Controller
     {
+        private readonly IHostingEnvironment _variable_env;
+        private readonly string _cheminVideo = "\\Videos\\video.mp4";
+
+
         public IActionResult Index()
         {
             return View();
@@ -41,6 +46,19 @@ namespace StreamClass.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public HomeController(IHostingEnvironment variableEnv)
+        {
+            _variable_env = variableEnv;
+        }
+
+        public IActionResult GetVideo()
+        {
+            string cheminVideoComplet = _variable_env.ContentRootPath + _cheminVideo;
+
+            FileStream stream = new FileStream(cheminVideoComplet, FileMode.Open);
+            return new FileStreamResult(stream, "video/mp4");
         }
     }
 }
